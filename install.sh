4381aa0b1e7a9e2be68d21dc2de50f2a415276f6
Leyo98765 <124131665+Leyo98765@users.noreply.github.com>
2023-02-07 16:25:00 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing PapyRadio dependencies"
+sudo apt-get update
+# Install the required packages via apt-get
+sudo apt-get -y install
+
+#required to end the plugin install
+echo "plugininstallend"
