e459e1ccb70c4127f3128e8cbbb434e1e53f7cfb
Leyo98765 <124131665+Leyo98765@users.noreply.github.com>
2023-02-07 16:24:24 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const re = require('socket.io-client');
+var frequence = 103.1;
+
+// Read the JSON file
+fs.readFile('./radios.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    // Parse the JSON data
+    const radioData = JSON.parse(data);
+
+    // Get the radio frequency passed as parameter
+    //const radioFrequency = process.argv[2];
+
+    // Search for the radio with the given frequency
+    let freqFM = frequence.toString() + " FM";
+    const radio = radioData.radios.find(radio => radio.frequency === freqFM);
+    console.log(radio);
+
+    // Check if the radio was found
+    if (radio) {
+        // Get the URL of the radio
+            const radioURL = radio.url;
+            const radioURlString = radioURL.toString();
+            console.log(radioURL);
+            // Play the radio using Volumio
+            const socket = re.io("http://volumio.local");
+            socket.emit('play', {"value": radioURlString});
+            console.log("Playing webRadio : " + radioURlString);
+            //socket.close();
+    } else {
+        console.error(`Radio with frequency ${freqFM} not found`);
+    }
+});
